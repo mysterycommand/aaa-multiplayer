f65080c7979ac39ab7e779a2d155ef0a64789327
Matt Hayes <matt@mysterycommand.com>
2023-10-24 14:54:20 +0200

    feat: client send and wire up pointer events

--- client/index.tsx
+++ client/index.tsx
@@ -21,6 +21,11 @@ Evt.from<Event>(ws, "open").attachOnce(async () => {
   negotiate(ws, pc, { RTCSessionDescription });
   const channel = pc.createDataChannel("aaa-multiplayer");
 
+  const clientSend = (action: Action) => {
+    useStore.dispatch(action);
+    channel.send(JSON.stringify(action));
+  };
+
   Evt.merge([
     Evt.from<Event>(channel, "close"),
     Evt.from<Event>(channel, "error"),
@@ -46,7 +51,60 @@ Evt.from<Event>(ws, "open").attachOnce(async () => {
     );
   }
 
-  channel.send(JSON.stringify(createAction("connect")));
+  Evt.merge([
+    Evt.from<PointerEvent>(document, "pointerenter", { capture: true }),
+    Evt.from<PointerEvent>(document, "pointerover", { capture: true }),
+    Evt.from<PointerEvent>(document, "pointerdown", { capture: true }),
+  ]).attach(({ buttons, pointerId, pointerType, x, y }) =>
+    clientSend(
+      createAction("pointerstart", {
+        pointerId,
+        pointerType,
+        isDown:
+          (pointerType === "mouse" && buttons !== 0) ||
+          pointerType === "touch" ||
+          pointerType === "pen",
+        x: x - window.innerWidth / 2,
+        y: y - window.innerHeight / 2,
+      }),
+    ),
+  );
+
+  Evt.merge([
+    Evt.from<PointerEvent>(document, "pointerleave", { capture: true }),
+    Evt.from<PointerEvent>(document, "pointerout", { capture: true }),
+    Evt.from<PointerEvent>(document, "pointerup", { capture: true }),
+  ]).attach(({ type, buttons, pointerId, pointerType, x, y }) =>
+    clientSend(
+      type === "pointerup" && pointerType === "mouse"
+        ? createAction("pointermove", {
+            pointerId,
+            pointerType,
+            isDown: buttons !== 0,
+            x: x - window.innerWidth / 2,
+            y: y - window.innerHeight / 2,
+          })
+        : createAction("pointerend", { pointerId }),
+    ),
+  );
+
+  Evt.from<PointerEvent>(document, "pointermove", { capture: true }).attach(
+    ({ buttons, pointerId, pointerType, x, y }) =>
+      clientSend(
+        createAction("pointermove", {
+          pointerId,
+          pointerType,
+          isDown:
+            (pointerType === "mouse" && buttons !== 0) ||
+            pointerType === "touch" ||
+            pointerType === "pen",
+          x: x - window.innerWidth / 2,
+          y: y - window.innerHeight / 2,
+        }),
+      ),
+  );
+
+  clientSend(createAction("connect"));
 });
 
 Object.defineProperty(window, "store", { value: useStore });
